165d26013f49535b478dd7cafb286fb8341a818e
Dmitrii Chernyshev <chernyshevddi@yandex.ru>
2022-01-20 03:22:48 +0300

    fix auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,11 +3,11 @@ const UnauthorizedError = require('../utils/errors/unauthorized');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { Authorization } = req.headers;
+  const { authorization } = req.headers;
   let payload;
 
   try {
-    payload = jwt.verify(Authorization, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(authorization, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
