fe7fcc6fa8c449a0d06fe42cbe95e8d5ccf4e6ef
Dmitrii Chernyshev <chernyshevddi@yandex.ru>
2022-01-23 22:28:21 +0300

    fix mdlwr auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,12 +4,11 @@ const UnauthorizedError = require('../utils/errors/unauthorized');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  console.log(authorization)
   let payload;
 
   try {
-    payload = jwt.verify(authorization, JWT_SECRET);
+    payload = jwt.verify(req.cookies.token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    console.log(payload)
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
